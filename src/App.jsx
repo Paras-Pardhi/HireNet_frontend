36d83b6827b5ac96b6f7cad27b21ccdf65adf7e3
Paras-Pardhi <paraspardhi98@gmail.com>
2025-08-04 17:17:58 +0530

    implmented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { lazy, Suspense, useContext, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login";
@@ -8,14 +8,23 @@ import axios from "axios";
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import Home from "./components/Home/Home";
-import Jobs from "./components/Job/Jobs";
-import JobDetails from "./components/Job/JobDetails";
-import Application from "./components/Application/Application";
-import MyApplications from "./components/Application/MyApplications";
-import PostJob from "./components/Job/PostJob";
+// import Jobs from "./components/Job/Jobs";
+// import JobDetails from "./components/Job/JobDetails";
+// import Application from "./components/Application/Application";
+// import MyApplications from "./components/Application/MyApplications";
+// import PostJob from "./components/Job/PostJob";
 import NotFound from "./components/NotFound/NotFound";
-import MyJobs from "./components/Job/MyJobs";
+// import MyJobs from "./components/Job/MyJobs";
 import { Context } from "./Contexts/GlobalContext";
+
+import Loading from "./Loader/Loading";
+
+const Jobs = lazy(() => import("./components/Job/Jobs"));
+const JobDetails = lazy(() => import("./components/Job/JobDetails"))
+const Application = lazy(() => import("./components/Application/Application"))
+const MyApplications = lazy(() => import("./components/Application/MyApplications"))
+const PostJob = lazy(() => import("./components/Job/PostJob"))
+const MyJobs = lazy(() => import("./components/Job/MyJobs"))
 
 const App = () => {
   const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
@@ -42,18 +51,20 @@ const App = () => {
     <>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/job/getall" element={<Jobs />} />
-          <Route path="/job/:id" element={<JobDetails />} />
-          <Route path="/application/:id" element={<Application />} />
-          <Route path="/applications/me" element={<MyApplications />} />
-          <Route path="/job/post" element={<PostJob />} />
-          <Route path="/job/me" element={<MyJobs />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<Loading />} >
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/job/getall" element={<Jobs />} />
+            <Route path="/job/:id" element={<JobDetails />} />
+            <Route path="/application/:id" element={<Application />} />
+            <Route path="/applications/me" element={<MyApplications />} />
+            <Route path="/job/post" element={<PostJob />} />
+            <Route path="/job/me" element={<MyJobs />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
         <Footer />
         <Toaster />
       </BrowserRouter>
---
+++ src/Loader/Loading.jsx
@@ -0,0 +1,12 @@
+
+import "./Loader.css"; 
+
+const Loading = () => {
+  return (
+    <div className="loader-container">
+      <div className="spinner" />
+    </div>
+  );
+};
+
+export default Loading;
